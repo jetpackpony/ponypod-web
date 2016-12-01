119dd7060bda7d00fc5de6060a8af7c501baf4be
jetpackpony <jetpackpony@gmail.com>
2016-12-01 16:59:36 +0300

    Add real images to podcasts testing

--- mirage/factories/podcast.js
+++ mirage/factories/podcast.js
@@ -1,10 +1,12 @@
-import { Factory } from 'ember-cli-mirage';
+import { faker, Factory } from 'ember-cli-mirage';
+
+const imageList = ['hi','99pi','cortex','freakonomics','serial','tal'];
 
 export default Factory.extend({
   title(i) {
     return `Podcast - ${i}`;
   },
   image(i) {
-    return `assets/images/${i}.png`;
+    return `assets/images/${faker.list.cycle(...imageList)(i)}.png`;
   }
 });
