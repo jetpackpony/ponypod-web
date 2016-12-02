5f2917890aee3aeb07289b23f65a56a90c6cec24
jetpackpony <jetpackpony@gmail.com>
2016-12-02 16:06:32 +0300

    Set show menu button as default

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   menuTitle: "PonyPod",
-  showBackArrow: true,
+  showBackArrow: false,
   actions: {
     onBackButtonClick() {
       console.log('back click');
