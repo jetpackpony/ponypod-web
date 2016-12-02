4571ba0eb6280fe3e74e952d286d132d50a0dc7a
jetpackpony <jetpackpony@gmail.com>
2016-12-02 16:07:39 +0300

    Conservate tests for showing/hiding side menu

--- tests/acceptance/side-menu-test.js
+++ tests/acceptance/side-menu-test.js
@@ -3,19 +3,7 @@ import moduleForAcceptance from 'ponypod-frontend/tests/helpers/module-for-accep
 
 moduleForAcceptance('Acceptance | side menu');
 
-skip('shows side menu when menu button is clicked', function(assert) {
-  visit('/');
-  find('#toggle-menu').click();
-
-  andThen(() => {
-    assert.ok(find('aside').css('left') >= 0, 'menu should be visible');
-    assert.equal(find('#body-shadow').css('display'), 'block', 'shadow should be visible');
-  });
-});
-
-skip('hides side menu when shadow is clicked', function(assert) {
-
-});
-
+skip('shows side menu when menu button is clicked');
+skip('hides side menu when shadow is clicked');
 skip('shows side menu when screen is swiped right');
 skip('hides side menu when screen is swiped left');
