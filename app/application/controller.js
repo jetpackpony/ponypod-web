57d93dc10abc7180407362fdde1f45d2fe334328
jetpackpony <jetpackpony@gmail.com>
2016-12-02 22:37:28 +0300

    Use navigation service properties instead of app controller properties

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  menuTitle: "PonyPod",
-  menuOpen: false,
-  showBackArrow: false,
-  actions: {
-    back() {
-      console.log('back click');
-    },
-    openMenu() {
-      this.set('menuOpen', true);
-    },
-    closeMenu() {
-      this.set('menuOpen', false);
-    }
-  }
+  navigation: Ember.inject.service()
 });
-
