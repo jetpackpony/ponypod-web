86cc76b2d84b4026b12d149f7212cc9f1ae827e0
jetpackpony <jetpackpony@gmail.com>
2016-12-02 16:07:08 +0300

    Add a test for show arrow button

--- tests/acceptance/nav-bar-test.js
+++ tests/acceptance/nav-bar-test.js
@@ -5,7 +5,6 @@ moduleForAcceptance('Acceptance | nav bar');
 
 test('is displayed', function(assert) {
   visit('/');
-
   andThen(function() {
     assert.equal(find('nav').length, 1, 'should display navbar');
   });
@@ -18,4 +17,12 @@ test('shows toggle menu button when on home page', function(assert) {
     assert.equal(find('#back-button').length, 0, 'should not show back button');
   });
 });
-skip('shows back arrow button when on inner page');
+
+test('shows back arrow button when on inner page', function(assert) {
+  server.create('podcast');
+  visit('/podcast/1');
+  andThen(() => {
+    assert.equal(find('#back-button').length, 1, 'should not show back button');
+    assert.equal(find('#toggle-menu').length, 0, 'should show menu button');
+  });
+});
