8c854f8849261df514e783ebb666d34d71fd1dce
jetpackpony <jetpackpony@gmail.com>
2016-12-02 22:51:21 +0300

    Add setup of navbar properties in routes

--- app/podcast/route.js
+++ app/podcast/route.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel() {
+    this.set('navigation.navTitle', 'Podcast route goes here');
+    this.set('navigation.showBackArrow', true);
+  }
 });
