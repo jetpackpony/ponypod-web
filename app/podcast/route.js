da4af1c040ac4aa4901cc0728f930bbf0e8155e5
jetpackpony <jetpackpony@gmail.com>
2016-12-03 19:30:06 +0300

    Add summary and description and image to model and factory

--- app/podcast/route.js
+++ app/podcast/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('podcast', params.podcast_id)
+    return this.get('store').findRecord('podcast', params.podcast_id);
   },
   afterModel(podcast) {
     this.set('navigation.navTitle', podcast.get('title'));
--- mirage/factories/podcast.js
+++ mirage/factories/podcast.js
@@ -1,12 +1,23 @@
 import { faker, Factory } from 'ember-cli-mirage';
 
-const imageList = ['hi','99pi','cortex','freakonomics','serial','tal'];
+const imageList = [
+ "http://static1.squarespace.com/static/52d66949e4b0a8cec3bcdd46/t/52ebf67fe4b0f4af2a4502d8/1391195777839/1500w/Hello+Internet.003.png",
+ "http://podcasts.howstuffworks.com/hsw/podcasts/sysk/sysk-audio-1600.jpg",
+ "https://media2.wnyc.org/i/1400/1400/l/80/1/wn16_wnycstudios_freakonomics-rev3.png",
+ "http://i.podster.fm/img/8436/crop/yes/size/default/8436-1.jpg",
+ "http://static1.squarespace.com/static/513abd71e4b0fe58c655c105/t/52c45a37e4b0a77a5034aa84/1388599866232/1500w/Artwork.jpg",
+ "http://golangshow.com/images/cover_big.png"
+];
 
 export default Factory.extend({
   title(i) {
-    return `Podcast - ${i}`;
+    return `Podcast - ${i + 1}`;
   },
-  image(i) {
-    return `assets/images/${faker.list.cycle(...imageList)(i)}.png`;
+  image: faker.list.cycle(...imageList),
+  summary() {
+    return faker.lorem.paragraph();
+  },
+  description() {
+    return faker.lorem.paragraph();
   }
 });
