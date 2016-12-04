7ad3714bc8084d1d5088f1dd39db0539fbea6da4
jetpackpony <jetpackpony@gmail.com>
2016-12-05 00:10:35 +0300

    Draft acceptance test for player

---
+++ tests/acceptance/player-test.js
@@ -0,0 +1,47 @@
+import { skip, test } from 'qunit';
+import moduleForAcceptance from 'ponypod-frontend/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | player');
+
+test('open mini player with episode', function(assert) {
+});
+
+skip('open mini player on episode page', function(assert) {
+});
+
+skip('player shows correct episode info', function(assert) {
+});
+
+skip('expand to maxi player when expand button clicked', function(assert) {
+});
+
+skip('collapse to mini player when collapse button clicked', function(assert) {
+});
+
+skip('shows correct info on maxi player', function(assert) {
+});
+
+skip('shows correct description on maxi player', function(assert) {
+});
+
+skip('goes to an episode page when click read more', function(assert) {
+});
+
+
+/* Playback things */
+
+
+skip('start playback of a chosen episode', function(assert) {
+});
+
+skip('pauses the playback when click pause', function(assert) {
+});
+
+skip('pauses the playback when hit pause on maxi player', function(assert) {
+});
+
+skip('rewinds 10s when rewind button clicked', function(assert) {
+});
+
+skip('forwards 30s when forward button clicked', function(assert) {
+});
