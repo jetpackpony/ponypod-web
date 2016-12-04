82c1a259881217a12e23b1715a6cb4f1380ec943
jetpackpony <jetpackpony@gmail.com>
2016-12-04 18:24:16 +0300

    Add episode model, facotory and relationships to podcast

---
+++ app/episode/model.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+import { belongsTo } from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  podcast: belongsTo('podcast'),
+  title: DS.attr('string'),
+  pubDate: DS.attr('date')
+});
--- app/podcast/model.js
+++ app/podcast/model.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
+import { hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   image: DS.attr('string'),
   summary: DS.attr('string'),
-  description: DS.attr('string')
+  description: DS.attr('string'),
+  episodes: hasMany('episode')
 });
