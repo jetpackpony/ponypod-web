d4e6b55f579f8390882e40dc4e6130cf6a68a4c7
jetpackpony <jetpackpony@gmail.com>
2016-12-04 19:59:07 +0300

    Add format-duration helper and tests

---
+++ tests/unit/helpers/format-duration-test.js
@@ -0,0 +1,19 @@
+import { formatDuration } from 'ponypod-frontend/helpers/format-duration';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format duration');
+
+test('it formats very long duration', function(assert) {
+  let result = formatDuration([20000]);
+  assert.equal(result, '5 hours 33 minutes', 'formatted output is incorrect');
+});
+
+test('it formats normal duration', function(assert) {
+  let result = formatDuration([2700]);
+  assert.equal(result, '45 minutes', 'formatted output is incorrect');
+});
+
+test('it formats very short duration', function(assert) {
+  let result = formatDuration([15]);
+  assert.equal(result, '15 seconds', 'formatted output is incorrect');
+});
