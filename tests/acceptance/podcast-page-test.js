66f835d0490805e499213fc1a03c48003c680a74
jetpackpony <jetpackpony@gmail.com>
2016-12-04 14:34:13 +0300

    Add acceptance tests for podcast page

--- tests/acceptance/podcast-page-test.js
+++ tests/acceptance/podcast-page-test.js
@@ -43,3 +43,26 @@ test('displays the podcast description', function(assert) {
     assert.equal(descr, 'Full description', 'description does not match');
   });
 });
+
+test('shows a list on podcast\'s episodes', function(assert) {
+  let podcast = server.create('podcast');
+  let episodes = server.createList('episode', { podcast }, 3);
+  visit('/podcast/1');
+  andThen(() => {
+    let epList = find('.episode-list .episode');
+    assert.equal(epList.length, 3, 'should show 3 episodes');
+    assert.equal(true, false, 'should show proper titles');
+  });
+});
+
+test('doesn\'t show other podcasts\' episodes', function(assert) {
+  let podcasts = server.createList('podcast', 2);
+  let ep1 = server.create('episode', { podcast: podcasts[0], title: "test-1" });
+  let ep2 = server.create('episode', { podcast: podcasts[1], title: "test-2" });
+  visit('/podcast/1');
+  andThen(() => {
+    let ep = find('.episode-list .episode');
+    assert.equal(epList.length, 1, 'should show 1 episodes');
+    assert.equal(find('.title', ep), 'test-1', 'should show proper episode');
+  });
+});
