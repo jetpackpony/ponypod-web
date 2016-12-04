25ff1fa6fa9e94ebdafd434b5e62931d90ceff8a
jetpackpony <jetpackpony@gmail.com>
2016-12-04 20:32:40 +0300

    Make better test data

--- mirage/factories/episode.js
+++ mirage/factories/episode.js
@@ -5,12 +5,12 @@ export default Factory.extend({
     return `Episode - ${i + 1}`;
   },
   pubDate() {
-    return faker.date.recent();
+    return faker.date.past();
   },
   duration() {
-    return faker.random.number(1, 10000);
+    return faker.random.number(10000);
   },
   description() {
-    return faker.lorem.paragraph();
+    return faker.lorem.paragraphs(10);
   }
 });
