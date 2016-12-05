65d23046b98a0ae731553ffd2e584d89b6d2543d
jetpackpony <jetpackpony@gmail.com>
2016-12-05 21:44:54 +0300

    Add position and progress to service

--- app/player/service.js
+++ app/player/service.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   playingEpisode: null,
   isPlaying: false,
-  showExpandedPlayer: false
+  showExpandedPlayer: false,
+  // The position in percent
+  progress: 0,
+  // The time in seconds
+  posision: 0
 });
