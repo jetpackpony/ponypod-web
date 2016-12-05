82e73d17d3b2e93eaf4a49b12e9b6d8dcf49a7c5
jetpackpony <jetpackpony@gmail.com>
2016-12-05 23:05:23 +0300

    Add play/pause button to episode page

--- app/components/episode-details/component.js
+++ app/components/episode-details/component.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['episode-details']
+  player: Ember.inject.service(),
+  classNames: ['episode-details'],
+  isPlaying: Ember.computed('player.{playingEpisode,isPlaying}', function() {
+    let epId = this.get('player.playingEpisode.id');
+    return epId === this.get('episode.id') && this.get('player.isPlaying');
+  }),
+  actions: {
+    play() {
+      this.set('player.playingEpisode', this.get('episode'));
+      this.set('player.isPlaying', true);
+    },
+    pause() {
+      this.set('player.isPlaying', false);
+    }
+  }
 });
