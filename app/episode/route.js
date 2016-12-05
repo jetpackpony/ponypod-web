fcfd4f9b54836277bf658e5ea4f6eac045d573ad
jetpackpony <jetpackpony@gmail.com>
2016-12-05 19:52:41 +0300

    Add auto show player on episode page

--- app/episode/route.js
+++ app/episode/route.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  player: Ember.inject.service(),
   model(params) {
     return this.get('store').findRecord('episode', params.episode_id);
   },
   afterModel(episode) {
+    this._setNavParams(episode);
+    this._setPlayingEpisode(episode);
+  },
+  _setNavParams(episode) {
     this.set('navigation.navTitle', episode.get('podcast').get('title'));
     this.set('navigation.showBackArrow', true);
+  },
+  _setPlayingEpisode(episode) {
+    if (!this.get('player.playingEpisode')) {
+      this.set('player.playingEpisode', episode);
+    }
   }
 });
--- tests/unit/episode/route-test.js
+++ tests/unit/episode/route-test.js
@@ -1,11 +1,49 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
+
+const navigationService = Ember.Service.extend({
+  menuOpen: false,
+  navTitle: "PonyPod",
+  showBackArrow: false,
+  back() { }
+}).create();
+
+const playerService = Ember.Service.extend({
+  playingEpisode: null,
+  isPlaying: false,
+  showExpandedPlayer: false
+}).create();
 
 moduleFor('route:episode', 'Unit | Route | episode', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+test('it sets navbar title to podcast name', function(assert) {
+  let route = this.subject({
+    navigation: navigationService
+  });
+  route._setNavParams(Ember.Object.create({
+    podcast: Ember.Object.create({ title: "testme" })
+  }));
+  assert.equal(navigationService.get('navTitle'), 'testme', 'sets the title');
+});
+
+test('it sets the current episode into player if none is playing', function(assert) {
+  let route = this.subject({
+    navigation: navigationService,
+    player: playerService
+  });
+  route._setPlayingEpisode(Ember.Object.create({ id: 1 }));
+  assert.equal(playerService.get('playingEpisode.id'), 1, 'episodes ids should match');
+});
+
+test('it does nothing if some episode is playing', function(assert) {
+  playerService.set('playingEpisode', Ember.Object.create({ id: 2 }));
+  let route = this.subject({
+    navigation: navigationService,
+    player: playerService
+  });
+  route._setPlayingEpisode(Ember.Object.create({ id: 1 }));
+  assert.equal(playerService.get('playingEpisode.id'), 2, 'playing episode should not have changed');
 });
