448a757267b6ef632f08dc431a08716bc2f620d1
jetpackpony <jetpackpony@gmail.com>
2016-12-06 15:08:08 +0300

    Add maxi player tests and implementation

--- app/components/maxi-player/component.js
+++ app/components/maxi-player/component.js
@@ -1,5 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['maxi-player']
+  player: Ember.inject.service(),
+  classNames: ['maxi-player'],
+  actions: {
+    collapsePlayer() {
+      this.set('player.showExpandedPlayer', false);
+    },
+    readMore() {
+      this.set('player.showExpandedPlayer', false);
+      this.get('transitionToEpisode')(this.get('player.playingEpisode'));
+    },
+    play() {
+      this.set('player.isPlaying', true);
+    },
+    pause() {
+      this.set('player.isPlaying', false);
+    },
+    rewind() {
+      this.get('player.rewind')(10);
+    },
+    forward() {
+      this.get('player.forward')(30);
+    }
+  }
 });
