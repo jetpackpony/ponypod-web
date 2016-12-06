f428a85d86a5839c184e66714f67ee0a1f6dbd96
jetpackpony <jetpackpony@gmail.com>
2016-12-06 19:46:56 +0300

    Move maxi-progress-bar styles to separate module

--- tests/integration/components/maxi-progress-bar/component-test.js
+++ tests/integration/components/maxi-progress-bar/component-test.js
@@ -25,19 +25,19 @@ moduleForComponent('maxi-progress-bar', 'Integration | Component | maxi progress
 
 test('it shows the playback progress', function(assert) {
   this.set('player.progress', 75);
-  this.render(hbs`{{maxi-player}}`);
-  let container = this.$('.maxi-player .progress').width();
-  let bar = this.$('.maxi-player .progress .determinate').width();
+  this.render(hbs`{{maxi-progress-bar}}`);
+  let container = this.$('.progress').width();
+  let bar = this.$('.progress .determinate').width();
   let diff = Math.abs(bar / container * 100 - 75);
   assert.ok(diff < 1, 'should be withing 1% of set progress');
 });
 
 test('it shows progress thumb on progress position', function(assert) {
   this.set('player.progress', 75);
-  this.render(hbs`{{maxi-player}}`);
-  let container = this.$('.maxi-player .progress').width();
-  let thumb = this.$('.maxi-player .thumb').position().left;
-  let thumbWidth = this.$('.maxi-player .thumb').outerWidth()
+  this.render(hbs`{{maxi-progress-bar}}`);
+  let container = this.$('.progress').width();
+  let thumb = this.$('.thumb').position().left;
+  let thumbWidth = this.$('.thumb').outerWidth()
   let diff = Math.abs((thumb + thumbWidth / 2) / container * 100 - 75);
   assert.ok(diff < 1, 'should be withing 1% of set progress');
 });
@@ -50,7 +50,7 @@ test('it calls jump to with value when progress bar clicked', function(assert) {
   this.set('player.jumpTo', (progress) => {
     assert.equal(progress, 33, 'should jump to the pointed progress');
   });
-  this.render(hbs`{{maxi-player}}`);
+  this.render(hbs`{{maxi-progress-bar}}`);
   let progress = this.$('.progress-wrapper');
   let x = progress.width() * .33 + progress.offset().left;
   let y = progress.height() / 2 + progress.offset().top;
