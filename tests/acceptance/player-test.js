1e4c8fc9c791a7cf757c3caa2d0b8d38af3405c7
jetpackpony <jetpackpony@gmail.com>
2016-12-06 15:08:52 +0300

    Add acceptance tests for maxi-player

--- tests/acceptance/player-test.js
+++ tests/acceptance/player-test.js
@@ -42,19 +42,69 @@ test('player shows correct episode info', function(assert) {
   });
 });
 
-skip('expand to maxi player when expand button clicked', function(assert) {
+test('expand to maxi player when expand button clicked', function(assert) {
+  let ep = server.create('episode');
+  visit('/episode/1');
+  click('.episode-details a.play');
+  click('#player .expand-player');
+  andThen(() => {
+    let miniPlayer = find('.mini-player:visible');
+    let maxiPlayer = find('.maxi-player:visible');
+    assert.equal(maxiPlayer.length, 1, 'maxi player is not visible');
+    assert.equal(miniPlayer.length, 0, 'mini player should not visible');
+  });
 });
 
-skip('collapse to mini player when collapse button clicked', function(assert) {
+test('collapse to mini player when collapse button clicked', function(assert) {
+  let ep = server.create('episode');
+  visit('/episode/1');
+  click('.episode-details a.play');
+  click('#player .expand-player');
+  click('#player .collapse-player');
+  andThen(() => {
+    let miniPlayer = find('.mini-player:visible');
+    let maxiPlayer = find('.maxi-player:visible');
+    assert.equal(maxiPlayer.length, 0, 'maxi player should not be visible');
+    assert.equal(miniPlayer.length, 1, 'mini player is not visible');
+  });
 });
 
-skip('shows correct info on maxi player', function(assert) {
+test('shows correct info on maxi player', function(assert) {
+  let podcast = server.create('podcast', { image: 'testme.png' });
+  let ep = server.create('episode', {
+    podcast,
+    title: 'Testme',
+    pubDate: new Date("Mon, 4 Nov 2016 9:57:12 +0000"),
+    duration: 7000,
+    summary: 'testme summary'
+  });
+  visit('/episode/1');
+  click('.episode-details a.play');
+  click('#player .expand-player');
+  andThen(() => {
+    let title = find('.maxi-player .title h2:visible').text().trim();
+    let date = find('.maxi-player .date:visible').text().trim();
+    let duration = find('.maxi-player .duration:visible').text().trim();
+    let summary = find('.maxi-player .duration p:visible').text().trim();
+    assert.equal(title, 'Testme', 'title should match');
+    assert.equal(date, '4 Nov 2016', 'date should match');
+    assert.equal(duration, '1 hour 56 minutes', 'duration should match');
+    assert.equal(summary, 'testme summary', 'duration should match');
+  });
 });
 
-skip('shows correct description on maxi player', function(assert) {
-});
-
-skip('goes to an episode page when click read more', function(assert) {
+test('goes to an episode page when click read more', function(assert) {
+  let podcast = server.create('podcast');
+  server.create('episode', { podcast });
+  visit('/podcast/1');
+  click('.episode a.play-button.play');
+  click('#player .expand-player');
+  click('#player .description .actions a');
+  andThen(() => {
+    let maxi = find('.maxi-player:visible').length === 0;
+    assert.ok(maxi, 'should not show maxi player');
+    assert.equal(currentURL(), '/episode/1', 'should be on episode page');
+  });
 });
 
 
