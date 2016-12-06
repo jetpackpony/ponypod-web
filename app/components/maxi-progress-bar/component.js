11b700e33bab2dcfcecfeccee70f6823da3c602c
jetpackpony <jetpackpony@gmail.com>
2016-12-06 18:58:52 +0300

    Add jump-to-click functionality to progress bar

--- app/components/maxi-progress-bar/component.js
+++ app/components/maxi-progress-bar/component.js
@@ -3,9 +3,31 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   player: Ember.inject.service(),
   classNames: ['playback-progress'],
+  didInsertElement() {
+    this._super(...arguments);
+    this.set(
+      'thumbWidth',
+      this.$('.progress-container .thumb').outerWidth()
+    );
+  },
+  _calculateProgress(pageX, offset, width) {
+    let pos = ((pageX - offset.left) / width * 100);
+    pos = (pos > 100) ? 100 : pos;
+    pos = (pos < 0) ? 0 : pos;
+    return pos;
+  },
+  thumbPosition: Ember.computed('player.progress', 'thumbWidth', function() {
+    let progress = this.get('player.progress');
+    let width = this.get('thumbWidth');
+    return `calc(${progress}% - ${width / 2}px)`;
+  }),
   actions: {
-    jumpTo(event) {
-
+    jumpTo(event, coords) {
+      let pageX = (event.pageX === undefined) ? coords.pageX : event.pageX;
+      let offset = this.$('.progress-wrapper').offset();
+      let width = this.$('.progress-wrapper').width();
+      let newProgress = this._calculateProgress(pageX, offset, width);
+      this.get('player').jumpTo(newProgress);
     }
   }
 });
--- tests/integration/components/maxi-progress-bar/component-test.js
+++ tests/integration/components/maxi-progress-bar/component-test.js
@@ -1,20 +1,60 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+const playerService = Ember.Service.extend({
+  playingEpisode: Ember.Object.create({
+    id: 2,
+    podcast: Ember.Object.create({ image: 'testme.png' }),
+    title: 'Testme',
+    pubDate: new Date("Mon, 4 Nov 2016 9:57:12 +0000"),
+    duration: 3000,
+    summary: 'Testme summary'
+  }),
+  isPlaying: false,
+  showExpandedPlayer: true
+});
+
 moduleForComponent('maxi-progress-bar', 'Integration | Component | maxi progress bar', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.register('service:player', playerService);
+    this.inject.service('player', { as: 'player' });
+    this.$().addClass('controls');
+  }
+});
+
+test('it shows the playback progress', function(assert) {
+  this.set('player.progress', 75);
+  this.render(hbs`{{maxi-player}}`);
+  let container = this.$('.maxi-player .progress').width();
+  let bar = this.$('.maxi-player .progress .determinate').width();
+  let diff = Math.abs(bar / container * 100 - 75);
+  assert.ok(diff < 1, 'should be withing 1% of set progress');
+});
+
+test('it shows progress thumb on progress position', function(assert) {
+  this.set('player.progress', 75);
+  this.render(hbs`{{maxi-player}}`);
+  let container = this.$('.maxi-player .progress').width();
+  let thumb = this.$('.maxi-player .thumb').position().left;
+  let thumbWidth = this.$('.maxi-player .thumb').outerWidth()
+  let diff = Math.abs((thumb + thumbWidth / 2) / container * 100 - 75);
+  assert.ok(diff < 1, 'should be withing 1% of set progress');
+});
+
+test('it shows the correct passed and negative time', function(assert) {
 });
 
 test('it calls jump to with value when progress bar clicked', function(assert) {
   assert.expect(1);
   this.set('player.jumpTo', (progress) => {
-    assert.equals(progress, 33, 'should jump to the pointed progress');
+    assert.equal(progress, 33, 'should jump to the pointed progress');
   });
   this.render(hbs`{{maxi-player}}`);
   let progress = this.$('.progress-wrapper');
-  let x = progress.width() * .33;
-  let y = progress.height() / 2;
-  progress.trigger('click', [x, y]);
+  let x = progress.width() * .33 + progress.offset().left;
+  let y = progress.height() / 2 + progress.offset().top;
+  progress.trigger('click', { pageX: x, pageY: y });
 });
 
 test('it calls jump-to with value when progress thumb dragged', function(assert) {
@@ -27,7 +67,4 @@ test('it diplays the time when dragging the progress thumb', function(assert) {
 });
 
 test('it diplays the time when touch-dragging the progress thumb', function(assert) {
-});
-
-test('it shows the correct passed and negative time', function(assert) {
 });
