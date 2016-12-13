d921e32b2ff7d4f3c6cbd7b7c83cb6635730ddcc
jetpackpony <jetpackpony@gmail.com>
2016-12-13 17:34:31 +0300

    Add open/close search tests and functionality

--- app/components/search-bar/component.js
+++ app/components/search-bar/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['nav-search-wrapper'],
+  actions: {
+    openSearch() {
+      this.$().parents('.nav-wrapper').addClass('search-open');
+    },
+    closeSearch() {
+      this.$().parents('.nav-wrapper').removeClass('search-open');
+    }
+  }
 });
--- tests/integration/components/search-bar/component-test.js
+++ tests/integration/components/search-bar/component-test.js
@@ -6,13 +6,31 @@ const navService = Ember.Service.extend({
 });
 
 moduleForComponent('search-bar', 'Integration | Component | search bar', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.$().addClass('nav-wrapper');
+  }
 });
 
 test('it shows search button', function(assert) {
+  this.render(hbs`{{search-bar}}`);
+  let searchButtonVisible = this.$('#open-search:visible').length === 1;
+  assert.ok(searchButtonVisible, 'search button should be visible');
 });
 
 test('it expands search bar on click on search button', function(assert) {
+  this.render(hbs`{{search-bar}}`);
+  this.$('#open-search').click();
+  let inputVisible = this.$('input#search:visible').length === 1;
+  assert.ok(inputVisible, 'search field should be visible');
+});
+
+test('it collapses search bar on click on close search', function(assert) {
+  this.render(hbs`{{search-bar}}`);
+  this.$('#open-search').click();
+  this.$('#close-search').click();
+  let inputInVisible = this.$('input#search:visible').length === 1;
+  assert.notOk(inputInVisible, 'search field should not be visible');
 });
 
 test('it calls a callback from nav service when submitted', function(assert) {
