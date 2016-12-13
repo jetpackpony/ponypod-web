c822f7d98bdbcba2c0e6d427694c220941c5a0f9
jetpackpony <jetpackpony@gmail.com>
2016-12-13 23:10:56 +0300

    Fix search acceptance tests

--- tests/acceptance/search-test.js
+++ tests/acceptance/search-test.js
@@ -9,14 +9,14 @@ test('shows podcasts matching search query', function(assert) {
   server.createList('podcast', 2);
   server.create('podcast', { title: "testme" });
   visit('/podcasts');
+  click('#open-search');
   fillIn('input#search', 'testme');
-  keyEvent('input#search', 'keypress', 13);
 
   andThen(function() {
-    let podNumber = find('.podcast').length
-    let podcast = find('.podcast a[title="testme"]').text().trim();
+    let podNumber = find('.podcast').length;
+    let podcast = find('.podcast a[title="testme"]').attr('title');
     assert.equal(podNumber, 1, 'should show only one podcast');
-    assert.equel(podcast, 'testme', 'podcast should be shown');
+    assert.equal(podcast, 'testme', 'podcast should be shown');
   });
 });
 
@@ -25,28 +25,28 @@ test('shows multi podcasts matching search query', function(assert) {
   server.create('podcast', { title: "test" });
   server.create('podcast', { title: "testme" });
   visit('/podcasts');
-  fillIn('input#search', 'testme');
-  keyEvent('input#search', 'keypress', 13);
+  click('#open-search');
+  fillIn('input#search', 'test');
 
   andThen(function() {
-    let podNumber = find('.podcast').length
-    let podcast1 = find('.podcast a[title="test"]').text().trim();
-    let podcast2 = find('.podcast a[title="testme"]').text().trim();
+    let podNumber = find('.podcast').length;
+    let podcast1 = find('.podcast a[title="test"]').attr('title');
+    let podcast2 = find('.podcast a[title="testme"]').attr('title');
     assert.equal(podNumber, 2, 'should show only two podcasts');
-    assert.equel(podcast1, 'test', 'podcast should be shown');
-    assert.equel(podcast2, 'testme', 'podcast should be shown');
+    assert.equal(podcast1, 'test', 'podcast should be shown');
+    assert.equal(podcast2, 'testme', 'podcast should be shown');
   });
 });
 
-test('shows a message if nothing was found', function(assert) {
+test('shows no podcasts if nothing is found', function(assert) {
   server.create('podcast', { title: "test" });
   server.create('podcast', { title: "testme" });
   visit('/podcasts');
+  click('#open-search');
   fillIn('input#search', 'ololol');
-  keyEvent('input#search', 'keypress', 13);
 
   andThen(function() {
-    let podNumber = find('.podcast').length
+    let podNumber = find('.podcast').length;
     assert.equal(podNumber, 0, 'should show zero podcast');
   });
 });
