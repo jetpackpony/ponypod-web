d7c37edd73180036c30226853f253fc73cfb95cc
jetpackpony <jetpackpony@gmail.com>
2016-12-14 16:30:11 +0300

    Extract controller search functionality into a mixin

--- app/podcasts/controller.js
+++ app/podcasts/controller.js
@@ -1,17 +1,4 @@
 import Ember from 'ember';
+import ControllerWithSearchMixin from 'ponypod-frontend/mixins/controller-with-search';
 
-export default Ember.Controller.extend({
-  navigation: Ember.inject.service(),
-  queryParams: ['search'],
-  search: null,
-  _updateSearchQuery() {
-    let query = this.get('navigation.searchQuery');
-    if (query.length <= 2) {
-      query = null;
-    }
-    this.set('search', query);
-  },
-  onSearchQueryChange: Ember.observer('navigation.searchQuery', function() {
-    Ember.run.debounce(this, this._updateSearchQuery, 300);
-  })
-});
+export default Ember.Controller.extend(ControllerWithSearchMixin);
--- tests/unit/podcasts/controller-test.js
+++ tests/unit/podcasts/controller-test.js
@@ -1,35 +1,4 @@
 import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
-const navigationService = Ember.Service.extend({
-  searchQuery: ''
-});
-
-let origDebounce = Ember.run.debounce;
-moduleFor('controller:podcasts', 'Unit | Controller | podcasts', {
-  beforeEach() {
-    Ember.run.debounce = function(target, func) {
-      func.call(target);
-    };
-  },
-  afterEach() {
-    Ember.run.debounce = origDebounce;
-  }
-});
-
-test('updates the query param when search query changes', function(assert) {
-  let controller = this.subject({
-    navigation: navigationService.create()
-  });
-  controller.set('navigation.searchQuery', 'testme');
-  assert.equal(controller.get('search'), 'testme', 'query param should match');
-});
-
-test('sets query param to null if query is less than 2 letters', function(assert) {
-  let controller = this.subject({
-    navigation: navigationService.create()
-  });
-  controller.set('navigation.searchQuery', 'testme');
-  controller.set('navigation.searchQuery', 'te');
-  assert.equal(controller.get('search'), null, 'query param should not change');
-});
+moduleFor('controller:podcasts', 'Unit | Controller | podcasts');
