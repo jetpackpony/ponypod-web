1c848a75a7c6f67e8dea64e092f904929480a724
jetpackpony <jetpackpony@gmail.com>
2017-11-28 20:53:20 +0300

    Refactor format-duration helper - make more functional

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -1,16 +1,45 @@
 import Ember from 'ember';
+import R from 'npm:ramda';
 
 export function formatDuration([value]) {
-  if (value < 60) {
-    return `${value} second${value > 1 ? 's' : ''}`;
-  } else if (value < 3600) {
-    let val = Math.round(value / 60);
-    return `${val} minute${value > 1 ? 's' : ''}`;
-  } else {
-    let h = Math.floor(value/3600);
-    let m = Math.round((value - h * 3600) / 60);
-    return `${h} hour${h > 1 ? 's' : ''} ${m} minute${m > 1 ? 's' : ''}`;
-  }
+  return R.compose(
+    chooseDurationString,
+    getHrsMinsSecs,
+  )(value);
 }
 
 export default Ember.Helper.helper(formatDuration);
+
+const chooseDurationString = ({ hours, mins, secs }) => (
+  (hours === 0 && mins === 0)
+  ? composeDurationString(secs, 'second')
+  : ((hours === 0)
+    ? composeDurationString(mins, 'minute')
+    : ([
+      composeDurationString(hours, 'hour'),
+      composeDurationString(mins, 'minute')
+    ].join(' ')))
+);
+
+const composeDurationString = (value, label) => (
+  `${value} ${pluralize(value, label)}`
+);
+
+const pluralize = (value, string) => (
+  `${string}${(value > 1) ? 's' : ''}`
+);
+
+const getHrsMinsSecs = (value) => ({
+  hours: getHours(value),
+  mins: getMinutes(value),
+  secs: getSeconds(value)
+});
+const getHours = (value) => (
+  Math.floor(value / 3600)
+);
+const getMinutes = (value) => (
+  Math.floor((value - getHours(value) * 3600) / 60)
+);
+const getSeconds = (value) => (
+  value - getHours(value) * 3600 - getMinutes(value) * 60
+);
