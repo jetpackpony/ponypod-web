e430d028f3db47205051d4548da89b3b4fda6747
jetpackpony <jetpackpony@gmail.com>
2017-11-28 19:33:00 +0300

    Refactor format-time helper - make more functional

--- app/helpers/format-time.js
+++ app/helpers/format-time.js
@@ -1,18 +1,43 @@
 import Ember from 'ember';
+import R from 'npm:ramda';
 
 export function formatTime([value]) {
-  let val = Math.round(Math.abs(value));
-  let pref = value >= 0 ? '' : '-';
-  if (val < 3600) {
-    let min = Math.floor(val / 60);
-    let sec = val - min * 60;
-    return `${pref}${min < 10 ? '0':''}${min}:${sec < 10 ? '0':''}${sec}`;
-  } else {
-    let hour = Math.floor(val / 3600);
-    let min = Math.floor((val - hour * 3600) / 60);
-    let sec = val - hour * 3600 - min * 60;
-    return `${pref}${hour}:${min < 10 ? '0':''}${min}:${sec < 10 ? '0':''}${sec}`;
-  }
+  return R.compose(
+    addSignToTimeString(value),
+    composeTimeString,
+    getHrsMinsSecs,
+    Math.round,
+    Math.abs
+  )(value);
 }
 
 export default Ember.Helper.helper(formatTime);
+
+const addSignToTimeString = R.curry((value, string) => (
+  `${(value >= 0) ? '' : '-'}${string}`
+));
+
+const composeTimeString = ({ hours, mins, secs }) => (
+  (hours === 0)
+  ? `${mins}:${secs}`
+  : `${hours}:${mins}:${secs}`
+);
+
+const getHrsMinsSecs = (value) => ({
+  hours: getHours(value),
+  mins: padWithZeros(getMinutes(value)),
+  secs: padWithZeros(getSeconds(value))
+});
+
+const getHours = (value) => (
+  Math.floor(value / 3600)
+);
+const getMinutes = (value) => (
+  Math.floor((value - getHours(value) * 3600) / 60)
+);
+const getSeconds = (value) => (
+  value - getHours(value) * 3600 - getMinutes(value) * 60
+);
+const padWithZeros = (value) => (
+  `${(value < 10) ? '0' : ''}${value}`
+);
