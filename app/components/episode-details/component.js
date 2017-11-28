34d01dab9391cdb337f4d8506a5b43083bd022ee
jetpackpony <jetpackpony@gmail.com>
2017-11-28 16:07:26 +0300

    Add progress bar and replay button to episode-details

--- app/components/episode-details/component.js
+++ app/components/episode-details/component.js
@@ -7,9 +7,48 @@ export default Ember.Component.extend({
     let epId = this.get('player.playingEpisode.id');
     return epId === this.get('episode.id') && this.get('player.isPlaying');
   }),
+  didReceiveAttrs() {
+    this._super(...arguments);
+    // Generate a computed property which watches localStorage
+    // changes for this episode specifically
+    Ember.defineProperty(
+      this,
+      'progress',
+      Ember.computed(
+        `player.localStorage.episodes.${this.get('episode.id')}`,
+        function() {
+          let progress = 0;
+          let ep = this.get(`player.localStorage.episodes.${this.get('episode.id')}`);
+          if (ep) {
+            progress = getProgress(ep.position, ep.duration);
+          }
+          return progress;
+        }
+      )
+    );
+    Ember.defineProperty(
+      this,
+      'progressCss',
+      Ember.computed('progress', function() {
+        return Ember.String.htmlSafe(`width: ${this.get('progress')}%`);
+      })
+    );
+    Ember.defineProperty(
+      this,
+      'isPlayed',
+      Ember.computed('progress', function() {
+        return this.get('progress') >= 99;
+      })
+    );
+  },
+
   actions: {
+    replay() {
+      this.get('player.playNewEpisode')(this.get('episode'), true);
+      this.get('player').play();
+    },
     play() {
-      this.get('player.playNewEpisode')(this.get('episode'));
+      this.get('player.playNewEpisode')(this.get('episode'), false);
       this.get('player').play();
     },
     pause() {
@@ -17,3 +56,7 @@ export default Ember.Component.extend({
     }
   }
 });
+
+const getProgress = (position, duration) => (
+  Math.round((position || 0) / (duration || 1) * 100)
+);
