38a5ed053d65fb35bfc0a34e2068b3fd060ba3bd
jetpackpony <jetpackpony@gmail.com>
2017-11-30 15:06:11 +0300

    Refactor maxi-progress-bar
      * add Ember.computedWithProps to easily create computed props
      * use Ember.computedWithProps in maxi-progress-bar
      * unify touch and mouse event handlers
      * extract some pure functions

--- app/components/maxi-progress-bar/component.js
+++ app/components/maxi-progress-bar/component.js
@@ -1,102 +1,113 @@
 import Ember from 'ember';
+import R from 'npm:ramda';
 
 export default Ember.Component.extend({
   player: Ember.inject.service(),
   classNames: ['playback-progress'],
   thumbWidth: 40,
+  tmpProgress: 0,
   didInsertElement() {
     this._super(...arguments);
     this.set('thumbWidth', this.$('.thumb').outerWidth());
-
     // Handle mouse thumb drag
-    let onMouseMove = (e, coords) => {
-      Ember.run(() => {
-        let pageX = (e.pageX === undefined) ? coords.pageX : e.pageX;
-        this.set('_updatingProgress', this._calculateProgress(pageX));
-      });
-    };
-    let onMouseUp = (e) => {
-      Ember.run(() => {
-        this.$('.thumb').removeClass('focus');
-        this.$(document).unbind('mousemove', onMouseMove);
-        this.$(document).unbind('mouseup', onMouseUp);
-        this.set('player.progress', this.get('_updatingProgress'));
-        Ember.run.later(this, function() {
-          this.set('_updatingProgress', false);
-        }, 500);
-      });
-    };
-    this.$('.thumb').on('mousedown', (e, coords) => {
-      Ember.run(() => {
-        e.preventDefault();
-        this.$('.thumb').addClass('focus');
-        this.$(document).on('mousemove', onMouseMove);
-        this.$(document).on('mouseup', onMouseUp);
-        let pageX = (e.pageX === undefined) ? coords.pageX : e.pageX;
-        this.set('_updatingProgress', this._calculateProgress(pageX));
-      });
+    this.$('.thumb').on('mousedown', (e) => {
+      this.thumbDragStart(e);
+      this.bindMouseEvents();
     });
-
     // Handle touch thumb drag
-    this.$('.thumb').on('touchstart', (e, coords) => {
-      Ember.run(() => {
-        e.stopPropagation();
-        this.$('.thumb').addClass('focus');
-        let touches = e.touches === undefined ? coords.touches : e.touches;
-        this.set('_updatingProgress', this._calculateProgress(touches[0].pageX));
-      });
-    });
-    this.$('.thumb').on('touchmove', (e, coords) => {
-      Ember.run(() => {
-        let touches = e.touches === undefined ? coords.touches : e.touches;
-        this.set('_updatingProgress', this._calculateProgress(touches[0].pageX));
-      });
-    });
-    this.$('.thumb').on('touchend', (e) => {
-      Ember.run(() => {
-        this.$('.thumb').removeClass('focus');
-        this.set('player.progress', this.get('_updatingProgress'));
-        Ember.run.later(this, function() {
-          this.set('_updatingProgress', false);
-        }, 500);
-      });
-    });
-
+    this.$('.thumb').on('touchstart', this.thumbDragStart);
+    this.$('.thumb').on('touchmove', this.thumbDragMove);
+    this.$('.thumb').on('touchend', this.thumbDragEnd);
   },
-  _calculateProgress(pageX) {
-    let offset = this.$('.progress-wrapper').offset();
-    let width = this.$('.progress-wrapper').width();
-    let pos = ((pageX - offset.left) / width * 100);
-    pos = (pos > 100) ? 100 : pos;
-    pos = (pos < 0) ? 0 : pos;
-    return pos;
-  },
-  _updatingProgress: false,
-  barProgress: Ember.computed('player.progress', '_updatingProgress', function() {
-    return this.get('_updatingProgress') || this.get('player.progress');
-  }),
-  barProgressCss: Ember.computed('barProgress', function() {
-    let progress = parseInt(this.get('barProgress'), 10);
-    return Ember.String.htmlSafe(`width: ${progress}%`);
-  }),
-  thumbPositionCss: Ember.computed('barProgress', function() {
-    let prog = parseInt(this.get('barProgress'), 10);
-    let width = parseInt(this.get('thumbWidth'), 10) / 2;
-    return Ember.String.htmlSafe(`left: calc(${prog}% - ${width}px)`);
-  }),
-  thumbTime: Ember.computed('_updatingProgress', function() {
-    let progress = this.get('_updatingProgress');
-    let duration = this.get('player.duration');
-    if (!progress) {
-      return 0;
-    } else {
-      return Math.round(duration * progress / 100);
-    }
-  }),
+  barProgress: Ember.computedWithProps(
+    'tmpProgress',
+    'player.progress',
+    (tmpProgress, playerProgress) => tmpProgress || playerProgress
+  ),
+  barProgressCss: Ember.computedWithProps(
+    'barProgress',
+    (progress) => Ember.String.htmlSafe(`width: ${progress}%`)
+  ),
+  thumbPositionCss: Ember.computedWithProps(
+    'barProgress',
+    'thumbWidth',
+    (progress, thumbWidth) => Ember.String.htmlSafe(
+      `left: calc(${progress}% - ${thumbWidth / 2}px)`
+    )
+  ),
+  thumbTime: Ember.computedWithProps(
+    'tmpProgress',
+    'player.duration',
+    (progress, duration) => Math.round(progress * duration / 100)
+  ),
   actions: {
-    jumpTo(e, coords) {
-      let pageX = (e.pageX === undefined) ? coords.pageX : e.pageX;
-      this.set('player.progress', this._calculateProgress(pageX));
+    jumpTo(e) {
+      this.updateTmpProgress(e);
+      this.updatePlayerProgress();
     }
+  },
+  init() {
+    this._super(...arguments);
+    this.thumbMouseUp = this.thumbMouseUp.bind(this);
+    this.thumbDragStart = this.thumbDragStart.bind(this);
+    this.thumbDragMove = this.thumbDragMove.bind(this);
+    this.thumbDragEnd = this.thumbDragEnd.bind(this);
+  },
+  thumbDragStart(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.$('.thumb').addClass('focus');
+    this.thumbDragMove(e);
+  },
+  thumbDragMove(e) {
+    this.updateTmpProgress(e);
+  },
+  thumbDragEnd() {
+    this.$('.thumb').removeClass('focus');
+    this.updatePlayerProgress();
+    Ember.run.later(this, function() {
+      this.updateTmpProgress({ pageX: 0 });
+    }, 500);
+  },
+  thumbMouseUp() {
+    this.thumbDragEnd();
+    this.unbindMouseEvents();
+  },
+  updateTmpProgress(event) {
+    this.set(
+      'tmpProgress',
+      calcProgress(
+        this.$('.progress-wrapper').offset().left,
+        this.$('.progress-wrapper').width(),
+        getXCoordFromEvent(event)
+      )
+    );
+  },
+  updatePlayerProgress() {
+    this.set('player.progress', this.get('tmpProgress'));
+  },
+  bindMouseEvents() {
+    this.$(document).on('mousemove', this.thumbDragMove);
+    this.$(document).on('mouseup', this.thumbMouseUp);
+  },
+  unbindMouseEvents() {
+    this.$(document).unbind('mousemove', this.thumbDragMove);
+    this.$(document).unbind('mouseup', this.thumbMouseUp);
   }
 });
+
+const getXCoordFromEvent = (e) => (
+  ((e.touches) ? e.touches[0] : e).pageX || 0
+);
+
+const trimValue = R.curry((min, max, value) => (
+  (value > max)
+  ? max
+  : ((value < min)
+    ? min
+    : value)
+));
+
+const calcProgress = (barOffset, barWidth, xCoordinate) => (
+  trimValue(0, 100, (xCoordinate - barOffset) / barWidth * 100)
+);
