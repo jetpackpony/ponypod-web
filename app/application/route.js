364214d85074a16b711eb4c82b5dd128183427fe
jetpackpony <jetpackpony@gmail.com>
2017-12-25 21:52:04 +0300

    Hide sidebar when transitioning to another route

--- app/application/route.js
+++ app/application/route.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Route.extend({
+  navigation: Ember.inject.service(),
   player: Ember.inject.service(),
   title: (tokens) => (
     `${tokens.join(' - ')} - ${ENV.APP.appTitle}`
@@ -9,6 +10,7 @@ export default Ember.Route.extend({
   actions: {
     willTransition() {
       this.set('player.showExpandedPlayer', false);
+      this.set('navigation.menuOpen', false);
     }
   }
 });
